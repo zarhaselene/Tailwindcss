bc193e2266aafa0675be5eaa72c3a09421a661d7
Zarha <zarhaselene@gmail.com>
2024-12-03 12:02:17 +0100

    toggle for search container and overlay

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 const themeIcon = document.getElementById("themeIcon");
 const themeContainer = document.getElementById("themeContainer");
+
 themeIcon.addEventListener("click", () => {
   themeContainer.classList.toggle("show");
   themeContainer.classList.toggle("hidden");
@@ -20,5 +21,26 @@ const closeBtn = document.getElementById("closeBtn");
 const menuIcons = document.getElementById("menuIcons");
 closeBtn.addEventListener("click", () => {
   miniMenu.classList.add("hidden");
+  overlay.classList.add("hidden");
+});
+
+const searchIcon = document.getElementById("searchIcon");
+const searchBtn = document.getElementById("searchBtn");
+const searchContainer = document.getElementById("searchContainer");
+const escapeBtn = document.getElementById("escapeBtn");
+
+searchIcon.addEventListener("click", () => {
+  searchContainer.classList.toggle("show");
+  searchContainer.classList.toggle("hidden");
+  overlay.classList.remove("hidden");
+});
+searchBtn.addEventListener("click", () => {
+  searchContainer.classList.toggle("show");
+  searchContainer.classList.toggle("hidden");
+  overlay.classList.remove("hidden");
+});
+escapeBtn.addEventListener("click", () => {
+  searchContainer.classList.toggle("show");
+  searchContainer.classList.toggle("hidden");
   overlay.classList.add("hidden");
 });
