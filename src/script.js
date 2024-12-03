990c48dec5b80991d4dbb18ee694519f7ba3d690
Zarha <zarhaselene@gmail.com>
2024-12-03 10:44:20 +0100

    Open and close mini menu functionality, show/hide overlay

--- src/script.js
+++ src/script.js
@@ -1,8 +1,24 @@
 const themeIcon = document.getElementById("themeIcon");
 const themeContainer = document.getElementById("themeContainer");
-
-//on click of theme icon, show the theme container with toggle: remove hidden class and and add show class
 themeIcon.addEventListener("click", () => {
   themeContainer.classList.toggle("show");
   themeContainer.classList.toggle("hidden");
+});
+
+const miniMenuBtn = document.getElementById("miniMenuBtn");
+const miniMenu = document.getElementById("miniMenu");
+const overlay = document.getElementById("blurOverlay");
+
+miniMenuBtn.addEventListener("click", () => {
+  miniMenu.classList.remove("hidden");
+  miniMenu.classList.add("show");
+  overlay.classList.remove("hidden");
+  overlay.classList.add("show");
+});
+
+const closeBtn = document.getElementById("closeBtn");
+const menuIcons = document.getElementById("menuIcons");
+closeBtn.addEventListener("click", () => {
+  miniMenu.classList.add("hidden");
+  overlay.classList.add("hidden");
 });
